fbdbeb63fd76e6c54bfecde30316088f25a16933
abishek <abi.rulezz@gmail.com>
2019-10-12 13:07:02 +0530

    Added lists.py

---
+++ lists.py
@@ -0,0 +1,30 @@
+shopping_list = ['apples', 'oranges', 'grapes', 'mangoes']
+print(shopping_list)
+
+# adding items to the list
+
+shopping_list.append('lemon')
+print(shopping_list)
+
+# removes item from the list
+
+shopping_list.remove('oranges')
+print(shopping_list)
+
+# removes last item in the list
+
+shopping_list.pop()
+print(shopping_list)
+
+# returns length of the list
+print(len(shopping_list))
+
+# getting a value from a list
+print(shopping_list[0])
+
+# getting last value from a list
+print(shopping_list[-1])
+
+# slicing a list
+print(shopping_list[0:2])
+print(shopping_list[1:3])
