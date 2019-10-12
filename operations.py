6deff948b914acf88adfbf97fe43f6fc32c71dd2
abishek <abi.rulezz@gmail.com>
2019-10-12 12:29:33 +0530

    Added operations.py

---
+++ operations.py
@@ -0,0 +1,18 @@
+a = 4
+
+# normal math operations
+
+b = a * 2
+c = a / 2
+d = a + 2
+e = a - 2
+f = a ** 2
+
+# shortcut math operations
+
+a *= 2  # a = a * 2
+a /= 2  # a = a / 2
+a += 2  # a = a + 2
+a -= 2  # a = a - 2
+
+print(f)
