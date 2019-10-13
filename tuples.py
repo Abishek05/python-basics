18cfd54bc0f9b89c268cc244fc5c774919a9cd4e
abishek <abi.rulezz@gmail.com>
2019-10-13 13:01:12 +0530

    Added tuples.py

---
+++ tuples.py
@@ -0,0 +1,13 @@
+device_mode = ('secureMode', 'normalMode')
+
+devices = ('iphone', 'ipad', 'imac')
+
+print(device_mode)
+
+# updating a tuple
+newTuple = device_mode + devices
+print(newTuple)
+
+# deleting tuples
+del devices
+print(devices)
