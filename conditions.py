996f1da4e3b24e38fdc82f48457e4d6644202e97
abishek <abi.rulezz@gmail.com>
2019-10-13 13:07:18 +0530

    Added conditions.py

---
+++ conditions.py
@@ -0,0 +1,18 @@
+bank_balance = 100
+
+if bank_balance > 400:
+    print("lets buy it")
+else:
+    print("we cant buy it")
+
+
+list = ['apples', 'oranges', 'grapes']
+
+if 'apples' in list:
+    print("Hello, apple is in the list")
+elif 'oranges' in list:
+    print('Hello, Orange is in the list')
+elif 'grapes' in list:
+    print('Hello, grapes is in the list')
+else:
+    print("Fruit is not in the list")
