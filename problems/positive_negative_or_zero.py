4bf63162dc7475c37dbbd06ce270bb0f2b3bc0cf
abishek <abi.rulezz@gmail.com>
2019-10-13 20:22:52 +0530

    Added problems/positive_negative_or_zero.py

---
+++ problems/positive_negative_or_zero.py
@@ -0,0 +1,8 @@
+value = float(input("Enter the number:"))
+
+if value > 0:
+    print("The number is positive")
+elif value < 0:
+    print("The number is negative")
+else:
+    print("The number is zero")
